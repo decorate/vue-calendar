53df3f8a1718414ac52c24f0a27e608d4f2b7fd9
ishizuka <ishizuka@shrp.jp>
2019-08-02 17:18:02 +0900

    first commit

---
+++ src/components/Example.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Example!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'example',
+
+        data() {
+            return {}
+        },
+
+        methods: {
+        }
+    }
+</script>
